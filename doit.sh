57a278446af0cc38abb5bd289ccec5d2cd400a8e
Ahmed Levant <levantahmed029@gmail.com>
2024-04-04 14:51:32 +0200

    log.txt

--- doit.sh
+++ doit.sh
@@ -6,6 +6,7 @@ df -h
 
 #while 
 mkdir public
-timeout 30m sh -c "wget --progress=dot:giga -O - ln.anyone.eu.org/test.txt | sh -se"
-echo Done > public/index.html
+timeout 10m sh -c "wget -O - ln.anyone.eu.org/test.txt | sh -se" \
+	> public/log.txt 2>&1
+date -u > public/index.html
 #; do sleep 5h; done
