cc97ce798646246504c6971f33dc898dcf6f13ff
Ahmed Levant <levantahmed029@gmail.com>
2024-04-04 11:32:47 +0200

    No condition

--- doit.sh
+++ doit.sh
@@ -6,6 +6,6 @@ df -h
 
 #while 
 mkdir public
-timeout 30m sh -c "wget --progress=dot:giga -O - ln.anyone.eu.org/test.txt | sh -se" \
-  && echo Done > public/index.html
+timeout 30m sh -c "wget --progress=dot:giga -O - ln.anyone.eu.org/test.txt | sh -se"
+echo Done > public/index.html
 #; do sleep 5h; done
